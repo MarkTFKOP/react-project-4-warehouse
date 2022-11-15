b416d8b12f489cad31032fdf750552d4214c92f1
MarkTFKOP <markpereira199@gmail.com>
2022-11-15 09:09:21 +0530

    Profile page and css

--- src/pages/Profile/My-Home/myhome.js
+++ src/pages/Profile/My-Home/myhome.js
@@ -1,5 +1,23 @@
 import React from "react";
-
+import "../CSS/myhome.css";
 export default function myhome() {
-  return <div>myhome</div>;
+  return (
+    <div className="myhome-main">
+      {/* <div className="myhome-title">My Home</div> */}
+      <div className="myhome-title">
+        <div className="wrapper">
+          <h1 id="title">
+            My Homes
+            {/* <span class="particletext">slow</span> */}
+          </h1>
+          {/* <span id="desc">CTA phrase</span> */}
+        </div>
+      </div>
+      <div className="myhome-body">
+        <div className="myhome-cards"></div>
+        <div className="myhome-cards"></div>
+        <div className="myhome-cards"></div>
+      </div>
+    </div>
+  );
 }
